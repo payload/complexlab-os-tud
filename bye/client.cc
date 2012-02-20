d5c6afd65ae574d841ff0f9dc37378283aeb9743
payload <payload@lavabit.com>
2012-02-20 11:00:24 +0100

    cleaned bye client from test-malloc code

--- bye/client.cc
+++ bye/client.cc
@@ -5,7 +5,6 @@
 #include <l4/cxx/ipc_stream>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 int bye_call(L4::Cap<void> const &server, const char *str)
 {
@@ -23,44 +22,6 @@ int bye_call(L4::Cap<void> const &server, const char *str)
 
 int main()
 {
-  // ad hoc test code for malloc
-
-  int *a, *b;
-
-  printf("\n# test 0 #\n");
-  a = new int(0xCAFE);
-  if (*a != 0xCAFE) return 1;
-  delete a;
-
-  printf("\n# test 1 #\n");
-  a = new int;
-  b = new int;
-  delete a;
-  delete b;
-
-  printf("\n# test 2 #\n");
-  a = new int;
-  b = new int;
-  delete a;
-  a = new int;
-  printf("\n# check #\n");
-  delete a;
-  delete b;
-
-  printf("\n# test 3 #\n");
-  a = new int[2000];
-  
-  printf("\n# test 4 #\n");
-  delete a;
-
-  printf("\n# test 5 #\n");
-  a = new int[2000];
-  b = new int[10000];
-  delete a;
-  delete b;
-  
-  //////////////////
-
   L4::Cap<void> server = L4Re::Env::env()->get_cap<void>("bye_server");
   if (!server.is_valid())
     {
