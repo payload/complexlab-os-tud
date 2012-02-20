f325104294372aed1986441a5a7bed52b2db7d1a
payload <payload@lavabit.com>
2012-02-20 17:18:45 +0100

    sessions and three hi and bye saying clients

--- bye/client.cc
+++ bye/client.cc
@@ -10,28 +10,32 @@ int bye_call(L4::Cap<void> const &server, const char *str)
 {
   L4::Ipc::Iostream s(l4_utcb());
   
-  unsigned long size = strlen(str)+1;
-  s << size << L4::Ipc::Buf_cp_out<const char>(str, size);
-  puts(str);
+  unsigned long size = strlen(str);
+  s << L4::Ipc::Buf_cp_out<const char>(str, size);
+  printf("%s!!\n", str);
   
   l4_msgtag_t res = s.call(server.cap());
-  if (l4_ipc_error(res, l4_utcb()))
-    return 1; // failure
-  return 0; // ok
+  if (l4_ipc_error(res, l4_utcb())) {
+    printf("ERROR while saying bye\n");
+    return 1;
+  }
+  return 0;
 }
 
 int main()
 {
   L4::Cap<void> server = L4Re::Env::env()->get_cap<void>("bye_server");
-  if (!server.is_valid())
-    {
-      printf("Could not get server capability!\n");
-      return 1;
-    }
-  if (bye_call(server, "bye!!"))
-    {
-      printf("Error talking to server\n");
-      return 1;
-    }
+  if (!server.is_valid()) {
+    printf("Could not get server capability!\n");
+    return 1;
+  }
+  if (bye_call(server, "hi")) {
+    printf("Error talking to server\n");
+    return 1;
+  }
+  if (bye_call(server, "bye")) {
+    printf("Error talking to server\n");
+    return 1;
+  }
   return 0;
 }
