e81c6dad899181ca48fd6828e1c0ea8e7651b176
payload <payload@lavabit.com>
2012-02-21 19:12:21 +0100

    omg, my size alignment was totally wrong. this test case shows it

--- test-malloc/main.cc
+++ test-malloc/main.cc
@@ -14,6 +14,11 @@ int main()
   if (*a != 0xCAFE) return 1;
   delete a;
 
+  printf("\n# test 0.5 #\n");
+  a = new int[3];
+  a[2] = 0;
+  delete a;
+
   printf("\n# test 1 #\n");
   a = new int;
   b = new int;
@@ -40,6 +45,6 @@ int main()
   b = new int[10000];
   delete a;
   delete b;
-  
+  /**/  
   return 0;
 }
