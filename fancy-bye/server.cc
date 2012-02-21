4bfb81f23e70fd054f680227cb1b0673fd2087bc
payload <payload@lavabit.com>
2012-02-22 00:56:16 +0100

    scrolling text!!

--- fancy-bye/server.cc
+++ fancy-bye/server.cc
@@ -12,6 +12,8 @@
 #include <l4/libgfxbitmap/bitmap.h>
 #include <l4/libgfxbitmap/font.h>
 
+#include <unistd.h>
+
 typedef unsigned long ulong;
 typedef unsigned char byte;
 
@@ -117,6 +119,10 @@ struct Gfx {
     fill(0, 0, _info.width, _info.height);
   }
 
+  void clear(ulong x, ulong y, ulong w, ulong h) {
+    _fill(x, y, w, h, _bg);
+  }
+
   void clear() {
     _fill(0, 0, _info.width, _info.height, _bg);
   }
@@ -132,13 +138,110 @@ struct Gfx {
   void bg(gfxbitmap_color_t bg) {
     _bg = convert_color(bg);
   }
+
+  ulong height() {
+    return _info.height;
+  }
+
+  ulong width() {
+    return _info.width;
+  }
+
+  ulong font_height() {
+    return gfxbitmap_font_height(GFXBITMAP_DEFAULT_FONT);
+  }
+};
+
+struct Line {
+  ulong nr;
+  Line *prev;
+  Line *next;
+  const char *text;
+
+  Line(ulong nr, const char *text)
+    : nr(nr), prev(NULL), next(NULL), text(text) {}
+  Line(ulong nr, const char *text, Line *prev)
+    : nr(nr), prev(prev), next(NULL), text(text) {}
+  Line(ulong nr, const char *text, Line *prev, Line *next)
+    : nr(nr), prev(prev), next(next), text(text) {}
+};
+
+struct TextView {
+  Line *top_line;
+  Line *lst_line;
+  Line *cur_line;
+  ulong lines;
+  
+  TextView()
+    : top_line(NULL), lst_line(NULL), cur_line(NULL), lines(0) {}
+
+  void append_line(const char *s) {
+    if (lst_line) {
+      lst_line->next = new Line(++lines, s, lst_line);
+      lst_line = lst_line->next;
+      cur_line = lst_line;
+    }
+    else {
+      lst_line = new Line(++lines, s);
+      top_line = lst_line;
+      cur_line = lst_line;
+    }
+  }
+
+  void draw(Gfx &gfx) {
+    if (!lines) return;
+    ulong y;
+    Line *line;
+    ulong line_height = gfx.font_height();
+    ulong lines_per_screen = gfx.height() / line_height;
+
+    while (top_line->nr > cur_line->nr)
+      top_line = top_line->prev;
+    
+    line = cur_line;
+    y = 1;
+    while (line) {
+      if (line == top_line)
+	break;
+      if (y == lines_per_screen)
+	break;
+      ++y;
+      line = line->prev;
+    }
+    top_line = line;
+
+    y = 0;
+    while (line && y < gfx.height()) {
+      if (line == cur_line) gfx.bg(0xDDDDDD);
+      gfx.clear(0, y, gfx.width(), line_height);
+      gfx.text(0, y, line->text);
+      if (line == cur_line) gfx.bg(0xFFFFFF);
+      y += line_height;
+      line = line->next;
+    }
+  }
 };
 
 int main()
 {
   printf("Let's do it!\n");
+
   Gfx gfx;
-  gfx.text(0, 0, "hi world!!");
+  TextView tv;
+
+  for (ulong i = 0; i < 50; ++i) {
+    char *s = new char[4];
+    sprintf(s, "%lu", i);
+    tv.append_line(s);
+    tv.draw(gfx);
+    usleep(50000);
+  }
+
+  while (tv.cur_line->nr > 1) {
+    tv.cur_line = tv.cur_line->prev;
+    tv.draw(gfx);
+    usleep(50000);
+  }
 
   SessionServer session;
   if (!server.registry()->register_obj(&session, "bye_server").is_valid())
