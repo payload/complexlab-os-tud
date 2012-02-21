fb8a79d54c7f6eb87b20f61d85f789715b490099
payload <payload@lavabit.com>
2012-02-21 19:49:29 +0100

    white screen

--- fancy-bye/server.cc
+++ fancy-bye/server.cc
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <cstring>
 #include <l4/re/env>
 #include <l4/re/util/cap_alloc>
 #include <l4/re/util/object_registry>
 #include <l4/re/util/meta>
 #include <l4/cxx/ipc_server>
+#include <l4/util/bitops.h>
 
 #include <l4/re/video/goos>
 #include <l4/re/util/video/goos_fb>
@@ -77,17 +79,67 @@ void print_goos_info(L4Re::Video::View::Info &info)
   //printf(" views    %u\n", info.num_static_views);
   //printf(" buffers  %u\n", info.num_static_buffers);
   printf(" bpp      %u\n", info.pixel_info.bits_per_pixel());
+  printf(" rsize    %u\n", info.pixel_info.r().size());
+  printf(" gsize    %u\n", info.pixel_info.g().size());
+  printf(" bsize    %u\n", info.pixel_info.b().size());
+  printf(" asize    %u\n", info.pixel_info.a().size());
+  printf(" rshift   %u\n", info.pixel_info.r().shift());
+  printf(" gshift   %u\n", info.pixel_info.g().shift());
+  printf(" bshift   %u\n", info.pixel_info.b().shift());
+  printf(" ashift   %u\n", info.pixel_info.a().shift());
   printf("==========\n");
 }
+
+typedef unsigned long ulong;
+typedef unsigned char byte;
+
+struct Color {
+  byte b;
+  byte g;
+  byte r;
+  Color(byte r, byte g, byte b) : b(b), g(g), r(r) {}
+};
+
+struct FB : public L4Re::Video::View::Info {
+  void *addr;
+  L4Re::Util::Video::Goos_fb goos;
+  ulong linesize;
+  ulong size;
+
+  FB() : goos("fb") {
+    goos.view_info(this);
+    linesize = l4util_next_power2(width * bpp());
+    size = linesize * height;
+  }
+
+  L4Re::Video::View::Info *info() const {
+    return (L4Re::Video::View::Info*)this;
+  }
+
+  unsigned char bpp() const {
+    return pixel_info.bytes_per_pixel();
+  }
+
+  Color *pxl(ulong x, ulong y) {
+    return (Color*)((ulong)addr + x*bpp() + y*linesize);
+  }
+
+  void draw_pixel(ulong x, ulong y, Color c) {
+    *(Color*)pxl(x,y) = c;
+  }
+
+  void refresh() {
+    goos.refresh(0, 0, width, height);
+  }
+};
 
 int main()
 {
   printf("Let's do it!\n");
-  L4Re::Util::Video::Goos_fb goos("fb");
-  L4Re::Video::View::Info info;
-  goos.view_info(&info);
-  print_goos_info(info);
-  //goos->attach_buffer();
+  FB fb;
+  fb.addr = fb.goos.attach_buffer();
+  print_goos_info(fb);
+  memset(fb.addr, 0xFF, fb.size);
 
   SessionServer session;
   if (!server.registry()->register_obj(&session, "bye_server").is_valid())
