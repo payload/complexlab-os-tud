d8e74793a9a9acf7819cc5154cbf064676ffaafb
payload <payload@lavabit.com>
2012-02-23 15:22:39 +0100

    map scancodes to printable keys and recognize shift

--- hacky-bye/server.cc
+++ hacky-bye/server.cc
@@ -75,6 +75,61 @@ using L4::Cap;
 using L4::Irq;
 using L4Re::Env;
 
+#define K_ERROR "\xff"
+#define K_ESC "\xff"
+#define K_BACKSPACE "\xff"
+#define K_LCTRL "\xff"
+#define K_LSHIFT "\xfe"
+#define K_RSHIFT "\xfe"
+#define K_KEYPAD_MUL "\xff"
+#define K_LALT "\xff"
+#define K_CAPSLOCK "\xff"
+#define K_F1 "\xff"
+#define K_F2 "\xff"
+#define K_F3 "\xff"
+#define K_F4 "\xff"
+#define K_F5 "\xff"
+#define K_F6 "\xff"
+#define K_F7 "\xff"
+#define K_F8 "\xff"
+#define K_F9 "\xff"
+#define K_F10 "\xff"
+#define K_NUMLOCK "\xff"
+#define K_SCROLLLOCK "\xff"
+#define K_KEYPAD_7 "\xff"
+#define K_KEYPAD_8 "\xff"
+#define K_KEYPAD_9 "\xff"
+#define K_KEYPAD_SUB "\xff"
+#define K_KEYPAD_4 "\xff"
+#define K_KEYPAD_5 "\xff"
+#define K_KEYPAD_6 "\xff"
+#define K_KEYPAD_ADD "\xff"
+#define K_KEYPAD_1 "\xff"
+#define K_KEYPAD_2 "\xff"
+#define K_KEYPAD_3 "\xff"
+#define K_KEYPAD_0 "\xff"
+#define K_KEYPAD_DOT "\xff"
+#define K_ALT_SYSRQ "\xff"
+#define K_UNKNOWN "\xff"
+#define K_F11 "\xff"
+#define K_F12 "\xff"
+
+const char *us_keymap =
+  K_ERROR K_ESC "1234567890-=" K_BACKSPACE "\tqwertyuiop[]\n" K_LCTRL "asdfghjkl;'`" K_LSHIFT // 2a
+  "\\zxcvbnm,./" K_RSHIFT K_KEYPAD_MUL K_LALT " " K_CAPSLOCK K_F1 K_F2 K_F3 K_F4 // 3e
+  K_F5 K_F6 K_F7 K_F8 K_F9 K_F10 K_NUMLOCK K_SCROLLLOCK K_KEYPAD_7 K_KEYPAD_8 // 48
+  K_KEYPAD_9 K_KEYPAD_SUB K_KEYPAD_4 K_KEYPAD_5 K_KEYPAD_6 K_KEYPAD_ADD // 4e
+  K_KEYPAD_1 K_KEYPAD_2 K_KEYPAD_3 K_KEYPAD_0 K_KEYPAD_DOT K_ALT_SYSRQ K_UNKNOWN // 55
+  K_UNKNOWN K_F11 K_F12; // 58
+
+const char *us_keymap_shift =
+  K_ERROR K_ESC "!\"§$%&/()=?" K_BACKSPACE "\tQWERTYUIOP{}\n" K_LCTRL "ASDFGHJKL;'~" K_LSHIFT // 2a
+  "|ZXCVBNM,.?" K_RSHIFT K_KEYPAD_MUL K_LALT " " K_CAPSLOCK K_F1 K_F2 K_F3 K_F4 // 3e
+  K_F5 K_F6 K_F7 K_F8 K_F9 K_F10 K_NUMLOCK K_SCROLLLOCK K_KEYPAD_7 K_KEYPAD_8 // 48
+  K_KEYPAD_9 K_KEYPAD_SUB K_KEYPAD_4 K_KEYPAD_5 K_KEYPAD_6 K_KEYPAD_ADD // 4e
+  K_KEYPAD_1 K_KEYPAD_2 K_KEYPAD_3 K_KEYPAD_0 K_KEYPAD_DOT K_ALT_SYSRQ K_UNKNOWN // 55
+  K_UNKNOWN K_F11 K_F12; // 58
+
 int main()
 {
   printf("Let's hit it!\n");
@@ -89,10 +144,23 @@ int main()
   l4_msgtag_t tag = irq->attach(1, env->main_thread());
   if (tag.has_error()) return print_error("ERROR irq->attach");
 
+  bool shift = false;
   for (;;) {
     irq->receive();
     byte s = l4util_in8(0x60);
-    printf("%x\n", s);
+    if (s == 0xff) continue; // error
+    bool release = s & 0x80;
+    if (release) s -= 0x80;
+    if (s <= 0x58) {
+      byte key = shift ? us_keymap_shift[s] : us_keymap[s];
+      if (key & 128) {
+	if (key == 0xfe)
+	  shift = !release;
+      } else if (!release) {
+	printf("%c", key);
+	fflush(stdout);
+      }
+    }
   }
 
   SessionServer session;
